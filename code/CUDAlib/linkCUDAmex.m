e8c309768c3fd0cd75a3b7ab6e7ea6bbe7f46379
Chris Thompson <chris@233analytics.com>
2020-11-13 08:33:15 -0500

    updated linker flags based on NPPI sublibraries

--- code/CUDAlib/linkCUDAmex.m
+++ code/CUDAlib/linkCUDAmex.m
@@ -5,9 +5,9 @@ if(nargin > 0)
     [myCodeHome, CUDAdirectory] = myPaths();
     objDir     = [myCodeHome '/code/CUDAlib/obj'];
     mexDir     = [myCodeHome '/code/CUDAlib/mex'];
-    
+
     %linkCUDAlibMex    = @(fName) mex('-cxx','-o', [mexDir '/' fName],'-v','-L', [CUDAdirectory '/lib64/'], '-l','cuda', '-l', 'cudart', '-l','cusparse', '-l','cublas', '-l','curand', [objDir '/' fName '.o']);
-    linkCUDAlibMex    = @(fName) mex('-cxx','-output', [mexDir '/' fName],'-v', '-lstdc++',['-L' CUDAdirectory '/lib64/'], '-lcuda', '-lcudart', '-lnppc', '-lnpps', '-lnppi', '-lcusparse', '-lcublas','-lcurand' , '-lmwblas',[objDir '/' fName '.o']);
+    linkCUDAlibMex    = @(fName) mex('-cxx','-output', [mexDir '/' fName],'-v', '-lstdc++',['-L' CUDAdirectory '/lib64/'], '-lcuda', '-lcudart', '-lnppc', '-lnpps', '-lnppial', '-lnppicc', '-lnppidei', '-lnppif', '-lnppig', '-lnppim', '-lnppist', '-lnppisu', '-lnppitc', '-lcusparse', '-lcublas','-lcurand' , '-lmwblas',[objDir '/' fName '.o']);
 
     if(~isdir(mexDir))
         mkdir(mexDir);
